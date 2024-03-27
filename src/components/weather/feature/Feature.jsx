69280c90725e84342ab02b704f7d56afeaab2b03
Doğukan Karasansar <karasansard@gmail.com>
2024-03-28 02:23:26 +0300

    Refactor weather feature and information components

--- src/components/weather/feature/Feature.jsx
+++ src/components/weather/feature/Feature.jsx
@@ -1,25 +1,27 @@
 import moment from "moment";
 
+const renderFeature = (forecast) => {
+  return (
+    <div className="flex flex-col items-center m-2">
+      <p className="text-sm font-medium text-sixth">
+        {moment(forecast.day).format("ddd")}
+      </p>
+      <img
+        className="w-12 h-12 rounded-full"
+        src={forecast.icon}
+        alt="Neil image"
+      />
+      <p className="text-sm font-medium text-white">{forecast.max}°c</p>
+      <p className="text-sm font-medium text-seventh">{forecast.min}°c</p>
+    </div>
+  );
+};
+
 export const Feature = ({ weather }) => {
   return (
     <div className="w-80 h-auto mt-2  p-2 rounded-lg shadow bg-third">
       <div className="grid grid-cols-5 gap-4">
-        {weather.five_day_forecast.map((forecast) => (
-          <>
-            <div className="flex flex-col items-center m-2">
-              <p className="text-sm font-medium text-sixth">
-                {moment(forecast.day).format("ddd")}
-              </p>
-              <img
-                className="w-12 h-12 rounded-full"
-                src={forecast.icon}
-                alt="Neil image"
-              />
-              <p className="text-sm font-medium text-white">{forecast.max}°c</p>
-              <p className="text-sm font-medium text-seventh">{forecast.min}°c</p>
-            </div>
-          </>
-        ))}
+        {weather.five_day_forecast.map((forecast) => renderFeature(forecast))}
       </div>
     </div>
   );
