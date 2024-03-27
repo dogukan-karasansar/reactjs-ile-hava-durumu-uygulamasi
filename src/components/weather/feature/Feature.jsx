c1e673cc7d62dd935661036047c502447ee403c8
Doğukan Karasansar <karasansard@gmail.com>
2024-03-28 02:21:57 +0300

    Add Feature component to Weather page

---
+++ src/components/weather/feature/Feature.jsx
@@ -0,0 +1,26 @@
+import moment from "moment";
+
+export const Feature = ({ weather }) => {
+  return (
+    <div className="w-80 h-auto mt-2  p-2 rounded-lg shadow bg-third">
+      <div className="grid grid-cols-5 gap-4">
+        {weather.five_day_forecast.map((forecast) => (
+          <>
+            <div className="flex flex-col items-center m-2">
+              <p className="text-sm font-medium text-sixth">
+                {moment(forecast.day).format("ddd")}
+              </p>
+              <img
+                className="w-12 h-12 rounded-full"
+                src={forecast.icon}
+                alt="Neil image"
+              />
+              <p className="text-sm font-medium text-white">{forecast.max}°c</p>
+              <p className="text-sm font-medium text-seventh">{forecast.min}°c</p>
+            </div>
+          </>
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/pages/weather/Weather.jsx
+++ src/pages/weather/Weather.jsx
@@ -2,11 +2,84 @@ import { useState } from "react";
 import Base from "../../layouts/base";
 import { Card } from "../../components/weather/card/Card";
 import { Information } from "../../components/weather/information/Information";
+import { Feature } from "../../components/weather/feature/Feature";
 
 export function Weather() {
   const [weathers, setWeathers] = useState([
     {
       id: 1,
+      country: "TR",
+      city: "Istanbul",
+      date: "2021-09-01",
+      temperature: "25",
+      weather: "Rain",
+      description: "Light rain",
+      icon: "/weather/icons/10d.svg",
+      banner: "/weather/backgrounds/10d.png",
+      temp_min: "22",
+      temp_max: "28",
+      details: [
+        {
+          key: "termal_sensation",
+          value: "25ºc",
+          label: "Termal Sensation",
+        },
+        {
+          key: "propable_rain",
+          value: "80%",
+          label: "Probability of rain",
+        },
+        {
+          key: "wind_speed",
+          value: "2 km/h",
+          label: "Wind speed",
+        },
+        {
+          key: "air_humidity",
+          value: "80%",
+          label: "Air humidity",
+        },
+        {
+          key: "uv_index",
+          value: "5",
+          label: "UV index",
+        },
+      ],
+      five_day_forecast: [
+        {
+          day: "2021-09-01",
+          min: "22",
+          max: "28",
+          icon: "/weather/icons/10d.svg",
+        },
+        {
+          day: "2021-09-02",
+          min: "22",
+          max: "28",
+          icon: "/weather/icons/10d.svg",
+        },
+        {
+          day: "2021-09-03",
+          min: "22",
+          max: "28",
+          icon: "/weather/icons/10d.svg",
+        },
+        {
+          day: "2021-09-04",
+          min: "22",
+          max: "28",
+          icon: "/weather/icons/10d.svg",
+        },
+        {
+          day: "2021-09-05",
+          min: "22",
+          max: "28",
+          icon: "/weather/icons/10d.svg",
+        },
+      ],
+    },
+    {
+      id: 2,
       country: "TR",
       city: "Istanbul",
       date: "2021-09-01",
@@ -88,19 +161,27 @@ export function Weather() {
 
   return (
     <Base>
-      {weathers.map((weather) => (
-        <div className="h-80 grid content-center justify-center">
-          <div className="flex flex-col h-56">
+      <div className={`grid grid-rows-${weathers.length} gap-4`}>
+        {weathers.map((weather, index) => (
+          <div className=" h-svh grid">
             <div
-              className="box-border h-72 w-80 p-4 rounded-xl bg-white shadow-lg"
-              style={styles(weather).banner}
+              key={index}
+              className="h-80 grid content-center justify-center"
             >
-              <Card weather={weather} />
+              <div className="flex flex-col h-56">
+                <div
+                  className="box-border h-72 w-80 p-4 rounded-xl bg-white shadow-lg"
+                  style={styles(weather).banner}
+                >
+                  <Card weather={weather} />
+                </div>
+                <Information weather={weather} />
+                <Feature weather={weather} />
+              </div>
             </div>
-            <Information weather={weather} />
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </Base>
   );
 }
